8d0fa5076ec3751d27caf47c9ac57db4b02ee3cc
sah-lishi <lishisaah@gmail.com>
2025-08-07 13:20:33 +0530

    api error

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed: ", err);
+})
+
+
+
 // const app = express()
 
 /*;( async () => {
